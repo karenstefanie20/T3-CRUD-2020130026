b836acc256b1c861cb5ac4e01d1dae25a8c0b724
Karen Stefanie Han's <113321232+karenstefanie20@users.noreply.github.com>
2022-12-15 14:53:56 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+oute::get('/books', [BookController::class,'index'])->name('books.index');
+Route::get('/books/create', [BookController::class,'create'])->name('books.create');
+Route::post('/books', [BookController::class,'store'])->name('books.store');
+Route::get('/books/{movie}', [BookController::class,'show'])->name('books.show');
+Route::get('/books/{movie}/edit', [BookController::class,'edit'])->name('books.edit');
+Route::patch('/books/{movie}', [BookController::class,'update'])->name('books.update');
+Route::delete('/books/{movie}', [BookController::class,'destroy'])->name('books.destroy');
